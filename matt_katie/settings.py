6543aec3a8581e420afd07ecc99c481a17f7381a
mchocianowski <ch.mateusz@hotmail.com>
2021-07-12 18:21:17 +0000

    add message storage for toasts.

--- matt_katie/settings.py
+++ matt_katie/settings.py
@@ -72,10 +72,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
@@ -154,5 +157,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+FREE_DELIVERY_THRESHOLD = 40
+STANDARD_DELIVERY_PERCENTAGE = 20
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
