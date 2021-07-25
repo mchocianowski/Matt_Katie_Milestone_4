48a884db032c93d44869f11f0fa609bd1482e73f
mchocianowski <ch.mateusz@hotmail.com>
2021-07-25 14:28:53 +0000

    remove admin path from contacts/urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from contact import views as contact_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('contact/', contact_views.contact_view, name='contact'),
+    path('', contact_views.contact_view, name='contact'),
     path('success/', contact_views.successView, name='success'),
 ]
